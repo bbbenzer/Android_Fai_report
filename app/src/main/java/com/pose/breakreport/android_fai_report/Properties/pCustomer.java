3942baf13832d02398c4b532268c1ca4bed25d39
benzsos <t_tofuz_bbbenz@hotmail.com>
2018-04-11 14:26:22 +0700

    เพิ่ม sublist

---
+++ app/src/main/java/com/pose/breakreport/android_fai_report/Properties/pCustomer.java
@@ -0,0 +1,27 @@
+package com.pose.breakreport.android_fai_report.Properties;
+
+/**
+ * Created by User on 11/4/2561.
+ */
+
+public class pCustomer {
+
+    private String Cus_Code;
+    private String Fullname;
+
+    public String getCus_Code() {
+        return Cus_Code;
+    }
+
+    public void setCus_Code(String cus_Code) {
+        Cus_Code = cus_Code;
+    }
+
+    public String getFullname() {
+        return Fullname;
+    }
+
+    public void setFullname(String fullname) {
+        Fullname = fullname;
+    }
+}
