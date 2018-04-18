742b8328c97548756bf943d694298b0c191ebeb8
benzsos <t_tofuz_bbbenz@hotmail.com>
2018-04-18 10:49:43 +0700

    เพิ่ม menu ใบสั่งผลิต

---
+++ app/src/main/java/com/pose/breakreport/android_fai_report/Order_bill_activity.java
@@ -0,0 +1,70 @@
+package com.pose.breakreport.android_fai_report;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.pose.breakreport.android_fai_report.Adapter.RoomAdapter;
+import com.pose.breakreport.android_fai_report.Properties.pRoom;
+
+import java.util.ArrayList;
+
+public class Order_bill_activity extends AppCompatActivity {
+
+    ArrayList<pRoom> PArray = new ArrayList<pRoom>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_bill_activity);
+
+        ArrayList menu = new ArrayList();
+        menu.add("พาย-ครัวซอง");
+        menu.add("ขนมปังไส้พี่ดาว");
+        menu.add("ขนมปังพี่บี พี่ลัย");
+        menu.add("ห้องแซนวิช");
+        menu.add("คุ้กกี้-ของกรอบ-ของแห้ง");
+        menu.add("ห้องขนมปังจืด");
+        menu.add("ขนมปังไส้พี่ดาว");
+        menu.add("ห้องพี่หอม");
+        menu.add("ห้องซอฟเค้ก");
+        menu.add("ห้องครัว-ไส้สังขยา");
+        menu.add("ห้องพายถ้วย ป้าเครือ");
+        menu.add("ห้องแต่งเค้ก");
+
+
+
+        for (int i=0;i<menu.size();i++){
+            pRoom Room = new pRoom();
+            Room.setRoomname(menu.get(i).toString());
+            PArray.add(Room);
+            Log.d("ASD", menu.get(i).toString());
+        }
+
+        ListView lv = (ListView) findViewById(R.id.list_room);
+        lv.setAdapter(new RoomAdapter(Order_bill_activity.this, PArray));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String choice = PArray.get(position).getRoomname();
+//                if(choice.equals("ใบสรุปขนมเบรค")){
+//                    Intent intent = new Intent(MainActivity_menu.this, Summary_break_activity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }else if(choice.equals("ใบสั่งผลิต")){
+////                    Intent intent = new Intent(MainActivity_menu.this, Activity_summary_break.class);
+////                    startActivity(intent);
+////                    finish();
+//                }else{
+//                    Log.d("Error", "Error enter list");
+//                }
+
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/pose/breakreport/android_fai_report/Properties/pRoom.java
@@ -0,0 +1,17 @@
+package com.pose.breakreport.android_fai_report.Properties;
+
+/**
+ * Created by User on 18/4/2561.
+ */
+
+public class pRoom {
+    private String roomname;
+
+    public String getRoomname() {
+        return roomname;
+    }
+
+    public void setRoomname(String roomname) {
+        this.roomname = roomname;
+    }
+}
